0627a26694560779581e98d9cc9a67d01cce0688
Diego G. Butori <diegogb.py@outlook.com>
2023-09-19 10:56:04 -0300

    Adding Exception Handler

--- FirstSteps/src/main/java/br/com/erudio/GreetingController.java
+++ FirstSteps/src/main/java/br/com/erudio/GreetingController.java
@@ -1,7 +1,6 @@
 package br.com.erudio;
 
 import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
